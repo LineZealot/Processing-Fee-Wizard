64f9b6f09fcacb9ed4b6dd5d41c9c619688361fc
Holden Smith <105118057+LineZealot@users.noreply.github.com>
2023-04-06 12:08:34 -0500

    background.js get processing fee function
    The script grabs the subtotal, multiplies it by 3.5% and rounds up by 0.01 then returns the amount

--- background.js
+++ background.js
@@ -1,19 +1,31 @@
 chrome.action.onClicked.addListener(async (tab) => {
-    await chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      function: () => {
-        const invoiceNum = (function(currUrl) {
-            const urlToArray = currUrl.split('/');
-            return urlToArray[5];
-        })(window.location.href);
-
-        let procFeeLink = 'a[href="/FrontDesk/InvoiceDetailAdd?DepartmentId=18&InvoiceId=' + invoiceNum +'"]';
-        const link = document.querySelector(procFeeLink);
-        if (link) {
-          window.location.href = link.href;
+    const tabId = tab.id;
+    // Get the receipt subtotal
+    const [result] = await chrome.scripting.executeScript({
+        target: {tabId},
+        func: () => {
+            const subTotal = parseFloat((document.querySelector("td.align-right.RunningTotal").textContent * 0.035) + 0.01).toFixed(2);
+            alert(subTotal);
+            return subTotal;
         }
-        alert(invoiceNum);
-        alert(procFeeLink);
-      }
+    });
+
+    // Open the processing fee page
+    await chrome.scripting.executeScript({
+        target: {tabId},
+        function: () => {
+            // Get the invoice number from the url
+            const invoiceNum = (function(currUrl) {
+                const urlToArray = currUrl.split('/');
+                return urlToArray[urlToArray.length - 1];
+            })(window.location.href);
+
+            // Ammends the link with the invoice number
+            let procFeeLink = 'a[href="/FrontDesk/InvoiceDetailAdd?DepartmentId=18&InvoiceId=' + invoiceNum +'"]';
+            const link = document.querySelector(procFeeLink);
+            if (link) {
+                window.location.href = link.href;
+            }
+        }
     });
   });
