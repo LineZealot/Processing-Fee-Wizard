e429b9bd27f43dea38ca2352be311ac07b78fe3b
Holden Smith <105118057+LineZealot@users.noreply.github.com>
2023-04-06 11:09:36 -0500

    Updated background.js
    The extension will now automatically activate the processing fee button on the invoice page with any invoice number

--- background.js
+++ background.js
@@ -2,10 +2,18 @@ chrome.action.onClicked.addListener(async (tab) => {
     await chrome.scripting.executeScript({
       target: {tabId: tab.id},
       function: () => {
-        const link = document.querySelector('a[href="/FrontDesk/InvoiceDetailAdd?DepartmentId=18]');
+        const invoiceNum = (function(currUrl) {
+            const urlToArray = currUrl.split('/');
+            return urlToArray[5];
+        })(window.location.href);
+
+        let procFeeLink = 'a[href="/FrontDesk/InvoiceDetailAdd?DepartmentId=18&InvoiceId=' + invoiceNum +'"]';
+        const link = document.querySelector(procFeeLink);
         if (link) {
           window.location.href = link.href;
         }
+        alert(invoiceNum);
+        alert(procFeeLink);
       }
     });
   });
