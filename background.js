1af9a160f122019bb3ae5b81b3c2bca50488bd3b
LineZealot <holdenmountain@gmail.com>
2023-04-07 18:14:01 -0500

    update background.js

--- background.js
+++ background.js
@@ -2,15 +2,59 @@ chrome.action.onClicked.addListener(async (tab) => {
 
     const tabId = tab.id;
     
-    // Get the receipt subtotal
-    const [result] = await chrome.scripting.executeScript({
+    // Check for old fee element
+    const [feeElement] = await chrome.scripting.executeScript({
         target: {tabId},
         func: () => {
-            const price = parseFloat((document.querySelector('td.align-right.RunningTotal').textContent * 0.035) + 0.01).toFixed(2);
-            return price;
+            const tdElements = [...document.querySelectorAll('td')];
+            return tdElements.some(td => td.textContent.trim() === 'Processing Fee');
         }
     });
-    console.log(result.result);
+    console.log(feeElement.result);
+    
+    // Get the old fee's price
+    if(feeElement.result === true) {
+        const[oldFeePrice] = await chrome.scripting.executeScript({
+            target: {tabId},
+            func: () => {
+                const rows = [...document.querySelector('table.InvoiceDetails > tbody').querySelectorAll('tr')];
+                const index = () => {
+                    for(let i = 0; i < rows.length; i++) {
+                        const cells = rows[i].getElementsByTagName('td');
+                        for (let j = 0; j < cells.length; j++) {
+                            if (cells[j].textContent.trim() === "Processing Fee") {
+                                return i;
+                            }
+                        }
+                    }
+                };
+                const price = () => {
+                    const rowIndex = index();
+                    const cells = rows[rowIndex + 3].getElementsByTagName('td');
+                    const num = parseFloat(cells[1].textContent);
+                    return num;
+                }
+                return price();
+            }
+        });
+        console.log(oldFeePrice.result);
+    }
+    
+
+    // Get the receipt subtotal
+    const [receipt] = await chrome.scripting.executeScript({
+        target: {tabId},
+        func: () => {
+            if(feeElement.result == true) {
+                const subTotal = parseFloat((document.querySelector('td.align-right.RunningTotal').textContent));
+                const newTotal = parseFloat(((subTotal - oldFeePrice.result) * 0.035) + 0.01).toFixed(2);
+                return newTotal;
+            } else {
+                const price = parseFloat((document.querySelector('td.align-right.RunningTotal').textContent * 0.035) + 0.01).toFixed(2);
+                return price;
+            }
+        }
+    });
 
     // Wait for the tab to navigate to a new page
     chrome.tabs.onUpdated.addListener(function listener(updatedTabId, changeInfo) {
@@ -21,14 +65,14 @@ chrome.action.onClicked.addListener(async (tab) => {
             // Execute a content script on the new page to autofill the number
             chrome.scripting.executeScript({
                 target: {tabId},
-                func: (result) => {
+                func: (receipt) => {
                     // Find the text input and fill in the number
                     const input = document.querySelector('input[type="text"]#txtListPrice');
                     if (input) {
-                        input.value = result.result.toString();
+                        input.value = receipt.result.toString();
                     }
                 },
-                args: [result]
+                args: [receipt]
             });
         }
     });
@@ -43,8 +87,14 @@ chrome.action.onClicked.addListener(async (tab) => {
                 return urlToArray[urlToArray.length - 1];
             })(window.location.href);
 
-            // Ammends the link with the invoice number
-            let procFeeLink = 'a[href="/FrontDesk/InvoiceDetailAdd?DepartmentId=18&InvoiceId=' + invoiceNum +'"]';
+            let procFeeLink = '';
+
+            //if(feeElement.result == true) {
+                procFeeLink = 'a[href="/FrontDesk/InvoiceDetailEdit?InvoiceDetailId=32728&InvoiceId=' + invoiceNum +'&entityRedirect="]';
+            //} else {
+            //    procFeeLink = 'a[href="/FrontDesk/InvoiceDetailAdd?DepartmentId=18&InvoiceId=' + invoiceNum +'"]';
+            //}
+
             const link = document.querySelector(procFeeLink);
             if (link) {
                 window.location.href = link.href;
